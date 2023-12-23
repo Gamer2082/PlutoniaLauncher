a7376a0d3cb067b93c06aff4da5da43ea711be41
Gamer2082 <revolutiongr4@gmail.com>
2023-12-23 10:35:45 +0000

    Update forge and avnia version

--- src/main/java/fr/plutonia/PlutoniaLauncher/Launcher.java
+++ src/main/java/fr/plutonia/PlutoniaLauncher/Launcher.java
@@ -16,7 +16,9 @@ import java.io.File;
 import java.nio.file.Path;
 
 public class Launcher {
-    private static GameInfos gameInfos = new GameInfos("PlutoniaLauncher",new GameVersion("1.20.1", GameType.V1_13_HIGHER_FORGE) , new GameTweak[]{GameTweak.FORGE});
+    private static final String ForgeVersion = "43.3.5";
+    private static final String VaniaVersion = "1.19.2";
+    private static GameInfos gameInfos = new GameInfos("PlutoniaLauncher",new GameVersion(ForgeVersion, GameType.V1_13_HIGHER_FORGE) , new GameTweak[]{GameTweak.FORGE});
     private static Path path = gameInfos.getGameDir();
     private static File crashFile = new File(String.valueOf(path),".crashReporter");
     private static CrashReporter crashReporter = new CrashReporter(String.valueOf(crashFile),path);
@@ -30,10 +32,10 @@ public class Launcher {
 
     }
     public static void update() throws Exception {
-        VanillaVersion vanillaVersion = new VanillaVersion.VanillaVersionBuilder().withName("1.20.1").build();
+        VanillaVersion vanillaVersion = new VanillaVersion.VanillaVersionBuilder().withName(VaniaVersion).build();
         UpdaterOptions updaterOptions = new UpdaterOptions.UpdaterOptionsBuilder().build();
 
-        AbstractForgeVersion version = new ForgeVersionBuilder(ForgeVersionBuilder.ForgeVersionType.NEW).withForgeVersion("47.2.18").build();
+        AbstractForgeVersion version = new ForgeVersionBuilder(ForgeVersionBuilder.ForgeVersionType.NEW).withForgeVersion(ForgeVersion).build();
 
         FlowUpdater updater = new FlowUpdater.FlowUpdaterBuilder().withVanillaVersion(vanillaVersion).withUpdaterOptions(updaterOptions).withModLoaderVersion(version).build();
         updater.update(path);
@@ -41,7 +43,7 @@ public class Launcher {
     }
     public static void launch() throws Exception {
         NoFramework noFramework = new NoFramework(path,authInfos,GameFolder.FLOW_UPDATER);
-        noFramework.launch("1.20.1","47.2.18", NoFramework.ModLoader.FORGE);
+        noFramework.launch(VaniaVersion,ForgeVersion, NoFramework.ModLoader.FORGE);
     }
 
     public static CrashReporter getCrashReporter() {
