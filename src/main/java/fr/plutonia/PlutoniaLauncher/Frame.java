29c77b1d8abec3324e9373f9c6246922e86b3a91
Gamer2082 <revolutiongr4@gmail.com>
2023-12-23 10:11:32 +0000

    Change version, add Launcher.java,add discord Rich and Frame.java

---
+++ src/main/java/fr/plutonia/PlutoniaLauncher/Frame.java
@@ -0,0 +1,26 @@
+package fr.plutonia.PlutoniaLauncher;
+
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
+
+public class Frame {
+    public static void main(String[] args) {
+        LaunchRpc();
+
+    }
+    public static void LaunchRpc(){
+        final DiscordRPC lib = DiscordRPC.INSTANCE;
+        final String appId = "1187921246978060409";
+        final String appToken = "e3qLqb7VCq0JdvghoMRO96uGw7_iko3f";
+        final DiscordEventHandlers eventHandlers = new DiscordEventHandlers();
+        lib.Discord_Initialize(appId,eventHandlers,true,"");
+        DiscordRichPresence richPresence = new DiscordRichPresence();
+        richPresence.startTimestamp = System.currentTimeMillis()/1000;
+        richPresence.details = "Joue a Plutonia, le nouveau serveur faction de référence. Réjoin nous :*\"https://discord.gg/gF7NEDCUwS\"* ou sur **Plutonia.fr**";
+        richPresence.state = "**Plutonia Launcher**";
+        richPresence.smallImageKey = "photo_de_profil";
+
+        lib.Discord_UpdatePresence(richPresence);
+    }
+}
--- src/main/java/fr/plutonia/PlutoniaLauncher/Launcher.java
+++ src/main/java/fr/plutonia/PlutoniaLauncher/Launcher.java
@@ -1,4 +1,50 @@
 package fr.plutonia.PlutoniaLauncher;
 
+import fr.flowarg.flowupdater.FlowUpdater;
+import fr.flowarg.flowupdater.utils.UpdaterOptions;
+import fr.flowarg.flowupdater.versions.AbstractForgeVersion;
+import fr.flowarg.flowupdater.versions.ForgeVersionBuilder;
+import fr.flowarg.flowupdater.versions.VanillaVersion;
+import fr.flowarg.openlauncherlib.NoFramework;
+import fr.litarvan.openauth.microsoft.MicrosoftAuthResult;
+import fr.litarvan.openauth.microsoft.MicrosoftAuthenticationException;
+import fr.litarvan.openauth.microsoft.MicrosoftAuthenticator;
+import fr.theshark34.openlauncherlib.minecraft.*;
+import fr.theshark34.openlauncherlib.util.CrashReporter;
+
+import java.io.File;
+import java.nio.file.Path;
+
 public class Launcher {
+    private static GameInfos gameInfos = new GameInfos("PlutoniaLauncher",new GameVersion("1.20.1", GameType.V1_13_HIGHER_FORGE) , new GameTweak[]{GameTweak.FORGE});
+    private static Path path = gameInfos.getGameDir();
+    private static File crashFile = new File(String.valueOf(path),".crashReporter");
+    private static CrashReporter crashReporter = new CrashReporter(String.valueOf(crashFile),path);
+    private static AuthInfos authInfos;
+
+    public static void Auth() throws MicrosoftAuthenticationException {
+        MicrosoftAuthenticator authenticator = new MicrosoftAuthenticator();
+        MicrosoftAuthResult result = authenticator.loginWithWebview();
+        authInfos = new AuthInfos(result.getProfile().getName(),result.getAccessToken(),result.getProfile().getId());
+
+
+    }
+    public static void update() throws Exception {
+        VanillaVersion vanillaVersion = new VanillaVersion.VanillaVersionBuilder().withName("1.20.1").build();
+        UpdaterOptions updaterOptions = new UpdaterOptions.UpdaterOptionsBuilder().build();
+
+        AbstractForgeVersion version = new ForgeVersionBuilder(ForgeVersionBuilder.ForgeVersionType.NEW).withForgeVersion("47.2.18").build();
+
+        FlowUpdater updater = new FlowUpdater.FlowUpdaterBuilder().withVanillaVersion(vanillaVersion).withUpdaterOptions(updaterOptions).withModLoaderVersion(version).build();
+        updater.update(path);
+
+    }
+    public static void launch() throws Exception {
+        NoFramework noFramework = new NoFramework(path,authInfos,GameFolder.FLOW_UPDATER);
+        noFramework.launch("1.20.1","47.2.18", NoFramework.ModLoader.FORGE);
+    }
+
+    public static CrashReporter getCrashReporter() {
+        return crashReporter;
+    }
 }
